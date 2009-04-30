1f0a0fc644859f80920a758467724dd5339c6f07
Mike Ho <mike@quasidea.com>
2009-04-29 23:00:19 -0700

    Completed dev harness

--- wwwroot/qtextstyle.php
+++ wwwroot/qtextstyle.php
@@ -3,11 +3,21 @@
 	
 	class QTextStyle extends QBaseClass {
 		public function DisplayAsHtml($strContent) {
-			print $strContent;
+			return $strContent;
 		}
 	}
 	
 	$objStyle = new QTextStyle();
 	$strContent = file_get_contents(dirname(__FILE__) . '/textism_sample.txt');
-	print $objStyle->DisplayAsHtml($strContent);
+	$strHtml = $objStyle->DisplayAsHtml($strContent);
 ?>
+
+	<h3 style="margin: 0;">Original</h3>
+	<div style="font: 11px lucida console; border: 1px solid black; overflow: auto; width: 800px; height: 200px; padding: 10px;"><?php print(nl2br(htmlentities($strContent))); ?></div>
+	<br/>
+	<h3 style="margin: 0;">Source HTML</h3>
+	<div style="font: 11px lucida console; border: 1px solid black; overflow: auto; width: 800px; height: 200px; padding: 10px;"><?php print(nl2br(htmlentities($strHtml))); ?></div>
+	<br/>
+	<h3 style="margin: 0;">Display HTML</h3>
+	<div style="font: 11px arial; border: 1px solid black; overflow: auto; width: 800px; height: 200px; padding: 10px;"><?php print($strHtml); ?></div>
+	
