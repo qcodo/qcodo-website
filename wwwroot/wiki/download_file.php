6e58595e45352c3a4b9d762e4633d01f36b575a1
Mike Ho <mike@quasidea.com>
2009-09-29 14:32:31 -0700

    Added wikifile download capability

---
+++ wwwroot/wiki/download_file.php
@@ -0,0 +1,12 @@
+<?php
+	require('../includes/prepend.inc.php');
+	$objWikiFile = WikiFile::LoadByWikiVersionId(QApplication::PathInfo(0));
+
+	if ($objWikiFile->FileName != QApplication::PathInfo(1))
+		QApplication::Redirect($objWikiFile->WikiVersion->WikiItem->UrlPath);
+
+	header('Content-Type: ' . $objWikiFile->FileMime);
+	header('Content-Length: ' . filesize($objWikiFile->GetPath()));
+	header('Content-Disposition: attachment; filename="' . $objWikiFile->FileName . '"');
+	print(file_get_contents($objWikiFile->GetPath()));
+?>
