0c7094423d627c024af22b49bd366449cdb01629
Mike Ho <mike@quasidea.com>
2009-10-03 15:45:26 -0700

    Basic view for qpm category

---
+++ wwwroot/qpm/category.php
@@ -0,0 +1,54 @@
+<?php
+	require('../includes/prepend.inc.php');
+
+	class QcodoForm extends QcodoWebsiteForm {
+		protected $strPageTitle = 'QPM Repository - ';
+		protected $intNavBarIndex = QApplication::NavGet;
+		protected $intSubNavIndex = QApplication::NavGetCommunity;
+
+		protected $objCategory;
+		protected $dtgPackages;
+
+		protected function Form_Create() {
+			parent::Form_Create();
+
+			$this->objCategory = PackageCategory::LoadByToken(QApplication::PathInfo(0));
+			if (!$this->objCategory) QApplication::Redirect('/qpm/');
+
+			$this->strPageTitle .= $this->objCategory->Name;
+			
+			$this->dtgPackages = new PackageDataGrid($this);
+			$this->dtgPackages->SetDataBinder('dtgPackages_Bind');
+			$this->dtgPackages->AlternateRowStyle->CssClass = 'alternate';
+
+			$this->dtgPackages->MetaAddColumn('Name', 'VerticalAlign=top', 'Width=250px');
+			$this->dtgPackages->MetaAddColumn('Token', 'Name=Path', 'VerticalAlign=top', 'Width=190px', 'HtmlEntities=false',
+				'Html=<a href="/qpm/package.php/<?= $_ITEM->Token; ?>" title="View Package Details"><?= $_ITEM->Token; ?></a>');
+			$this->dtgPackages->MetaAddColumn('Description', 'CssClass=small', 'Width=300px');
+			$this->dtgPackages->MetaAddColumn('LastPostDate', 'Name=Last Upload', 'Width=100px', 'VerticalAlign=top', 'CssClass=small');
+			$this->dtgPackages->MetaAddColumn(QQN::Package()->LastPostedByPerson->DisplayName, 'Name=By', 'Html=<?= $_FORM->RenderPostedBy($_ITEM); ?>', 'HtmlEntities=false', 'Width=100px', 'CssClass=small reverseLink', 'VerticalAlign=top');
+			
+			$this->dtgPackages->Paginator = new QPaginator($this->dtgPackages);
+		}
+
+		protected function dtgPackages_Bind() {
+			$objCondition = QQ::Equal(QQN::Package()->PackageCategoryId, $this->objCategory->Id);
+
+			$this->dtgPackages->TotalItemCount = Package::QueryCount($objCondition);
+
+			$objClauses = array();
+			if ($objClause = $this->dtgPackages->LimitClause)
+				$objClauses[] = $objClause; 
+			if ($objClause = $this->dtgPackages->OrderByClause)
+				$objClauses[] = $objClause; 
+
+			$this->dtgPackages->DataSource = Package::QueryArray($objCondition, $objClauses);
+		}
+
+		public function RenderPostedBy(Package $objPackage) {
+			return sprintf('<a href="%s">%s</a>', $objPackage->LastPostedByPerson->ViewProfileUrl, $objPackage->LastPostedByPerson->DisplayName);
+		}
+	}
+
+	QcodoForm::Run('QcodoForm');
+?>
---
+++ wwwroot/qpm/category.tpl.php
@@ -0,0 +1,20 @@
+<?php require(__INCLUDES__ . '/header.inc.php'); ?>
+
+	<div class="searchBar">
+		<div class="title">
+			<span class="subtitle">QPM Repository / </span>
+			<?php _p($this->objCategory->Name); ?>
+		</div>
+		<div class="right wiki">
+			<a href="/qpm/">Back to <strong>All Repositories</strong></a>
+		</div>
+	</div>
+
+	<div style="background-color: #ddd; padding: 5px 10px; border-bottom: 1px solid #999;">
+		<?php _p($this->objCategory->Description); ?>
+	</div>
+	<br/>
+
+	<?php $this->dtgPackages->Render(); ?>
+
+<?php require(__INCLUDES__ . '/footer.inc.php'); ?>
