fd33eee673959cd27c65667cb0837daad348e829
Mike Ho <mike@quasidea.com>
2009-10-15 21:35:26 -0700

    Implemented Login and Package getter in QPM webservice

--- www/_qpm/1_0.php
+++ www/_qpm/1_0.php
@@ -13,6 +13,30 @@
 			if ($blnDevelopment) return trim(file_get_contents(__QCODO_BUILDS__ . '/DEVELOPMENT'));
 			return trim(file_get_contents(__QCODO_BUILDS__ . '/STABLE'));
 		}
+
+		/**
+		 * Looks up a package given the PackageName and returns the ID if it exists, 0 otherwise
+		 * @param string $strPackageName
+		 * @return integer
+		 */
+		public function GetPackageId($strPackageName) {
+			$objPackage = Package::LoadByToken(trim(strtolower($strPackageName)));
+			if ($objPackage) return $objPackage->Id;
+			return 0;
+		}
+
+		/**
+		 * Given a Qcodo.com username and password, this will either return the user ID or 0 if the credentials are invalid
+		 * @param string $strUsername
+		 * @param string $strPassword
+		 * @return integer
+		 */
+		public function Login($strUsername, $strPassword) {
+			$objPerson = Person::LoadByUsername(trim(strtolower($strUsername)));
+			if (!$objPerson) return 0;
+			if ($objPerson->IsPasswordValid($strPassword)) return $objPerson->Id;
+			return 0;
+		}
 	}
 
 	if (QApplication::PathInfo(0)) {
@@ -21,6 +45,13 @@
 		switch (QApplication::PathInfo(0)) {
 			case 'GetCurrentQcodoVersion':
 				print $objService->GetCurrentQcodoVersion(QApplication::QueryString('dev'));
+				break;
+			case 'Login':
+				print $objService->Login(QApplication::QueryString('u'), QApplication::QueryString('p'));
+				break;
+			case 'GetPackageId':
+				print $objService->GetPackageId(QApplication::QueryString('name'));
+				break;
 		}
 	} else
 		QpmService::Run('QpmService');
