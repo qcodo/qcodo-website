057d8a5946e35a46a1d66121c288694412fbc37a
Mike Ho <mike@quasidea.com>
2009-10-15 20:57:06 -0700

    implemented qcodo version checker in qpm webservice

--- www/_qpm/1_0.php
+++ www/_qpm/1_0.php
@@ -2,10 +2,26 @@
 	require(dirname(__FILE__) . '/../../includes/prepend.inc.php');
 	
 	class QpmService extends QSoapService {
-		
+		/**
+		 * Returns the current Qcodo version, for stable or development
+		 * Returns a string formatted as int.int.int (three integers, separated by two periods)
+		 * representing the major version number, minor version number, and build number
+		 * @param boolean $blnDevelopment
+		 * @return string
+		 */
+		public function GetCurrentQcodoVersion($blnDevelopment) {
+			if ($blnDevelopment) return trim(file_get_contents(__QCODO_BUILDS__ . '/DEVELOPMENT'));
+			return trim(file_get_contents(__QCODO_BUILDS__ . '/STABLE'));
+		}
 	}
 
 	if (QApplication::PathInfo(0)) {
+		$objService = new QpmService('QpmService', null);
+
+		switch (QApplication::PathInfo(0)) {
+			case 'GetCurrentQcodoVersion':
+				print $objService->GetCurrentQcodoVersion(QApplication::QueryString('dev'));
+		}
 	} else
 		QpmService::Run('QpmService');
 ?>
