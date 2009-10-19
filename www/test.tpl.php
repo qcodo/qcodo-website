2584c254eaa5db65e3afb390fdb76f83af13855f
Mike Ho <mike@quasidea.com>
2009-10-19 15:00:00 -0700

    Cleaned up out of date files

--- www/test.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php require(__INCLUDES__ . '/header.inc.php'); ?>
-	<p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p>
-	<p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p>
-	<p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p><p>hello, world!</p>
-<?php require(__INCLUDES__ . '/footer.inc.php'); ?>
