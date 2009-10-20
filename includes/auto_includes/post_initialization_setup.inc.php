8a6cc47ebf092ac2c03106d9136721be50a60b9c
Mike Ho <mike@quasidea.com>
2009-10-19 22:32:30 -0700

    Cleaned up CLI directory with new 0.4.1 structure

--- includes/auto_includes/post_initialization_setup.inc.php
+++ includes/auto_includes/post_initialization_setup.inc.php
@@ -3,8 +3,8 @@
 		ini_set('include_path', '.:' . __INCLUDES__);
 		require('Zend/Search/Lucene.php');
 
-		// Pull from Session
-		QApplication::InitializePerson();
+		// Pull from Session (if applicable)
+		if (!QApplication::$CliMode) QApplication::InitializePerson();
 
 		// Other Setup
 		QMimeType::$MagicDatabaseFilePath = MIME_MAGIC_PATH;
