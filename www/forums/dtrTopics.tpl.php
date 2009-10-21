e0e85f619d51177e7997659cb5d21fe0ddfacc6f
Mike Ho <mike@quasidea.com>
2009-10-21 11:46:07 -0700

    fix to pesky topics list display issue for IE

--- www/forums/dtrTopics.tpl.php
+++ www/forums/dtrTopics.tpl.php
@@ -1,4 +1,4 @@
 <div class="<?php _p($_FORM->RenderTopicCss($_ITEM)); ?>" title="<?php _p($_ITEM->SidenavTitle); ?>" onmouseover="topicOver(this);" onmouseout="topicOut(this);" onclick="document.location='<?php _p($_FORM->RenderTopicLink($_ITEM)); ?>'">
 	<div class="title"><a href="<?php _p($_FORM->RenderTopicLink($_ITEM)); ?>" title="<?php _p($_ITEM->SidenavTitle); ?>"><?php _p(QString::Truncate($_ITEM->Name, ($_ITEM->IsViewed() ? 30 : 25)), false); ?></a></div>
-	<div class="count"><a href="<?php _p($_FORM->RenderTopicLink($_ITEM)); ?>" title="<?php _p($_ITEM->SidenavTitle); ?>"><?php _p($_ITEM->MessageCount);?></a></div>
+	<div class="count"><?php _p($_ITEM->MessageCount);?></div>
 </div>
