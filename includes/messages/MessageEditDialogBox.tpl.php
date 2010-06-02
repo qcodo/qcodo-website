f31895a88fa4b72c332e295164d79514ff1892d4
Mike Ho <mike@quasidea.com>
2010-06-01 23:01:01 -0700

    Open QTextStyle formatting in a new window

--- includes/messages/MessageEditDialogBox.tpl.php
+++ includes/messages/MessageEditDialogBox.tpl.php
@@ -3,7 +3,7 @@
 
 	<?php $_CONTROL->lstForum->RenderForDialog('Width=400px'); ?>
 	<?php $_CONTROL->txtTopicName->RenderForDialog('CssClass=forumTopicName textbox'); ?>
-	<?php $_CONTROL->txtMessage->RenderForDialog('CssClass=forumMessage','Instructions=To apply formatting, use <a href="/wiki/qcodo/qtextstyle">QTextStyle Formatting</a>.'); ?>
+	<?php $_CONTROL->txtMessage->RenderForDialog('CssClass=forumMessage','Instructions=To apply formatting, use <a href="/wiki/qcodo/qtextstyle" target="_blank">QTextStyle Formatting</a>.'); ?>
 	
 	<?php $_CONTROL->btnOkay->Render(); ?>
 	&nbsp;or&nbsp;
