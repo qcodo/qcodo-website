f36478794dcf4c83158e6c7904f95e4e711f92ff
Mike Ho <mike@quasidea.com>
2011-08-16 19:40:57 -0700

    Updated to latest stable (0.4.22) build of Qcodo

--- includes/qcodo/_core/framework/QApplicationBase.class.php
+++ includes/qcodo/_core/framework/QApplicationBase.class.php
@@ -684,7 +684,9 @@
 		 */
 		public static function Redirect($strLocation) {
 			// Clear the output buffer (if any)
-			ob_clean();
+			while (count(ob_list_handlers())) {
+				ob_end_clean();
+			}
 
 			if ((QApplication::$RequestMode == QRequestMode::Ajax) ||
 				(array_key_exists('Qform__FormCallType', $_POST) &&
